78b81aafd6ebbacb0435ab2c3a94f3b21cc9f1e3
11390091 <11390091@NUIGIT561.it.nuigalway.ie>
2014-11-26 10:42:21 +0000

    Testing git with comments added to tweetAggregation file
    Change-Id: Ifc411d4180ff78b78891f86c61863582ba206255

--- twitter_fetch/src/TweetAggregation.java
+++ twitter_fetch/src/TweetAggregation.java
@@ -10,7 +10,7 @@ public class TweetAggregation {
 		String line = "";
 		try {
 			// Load file to aggregate
-			BufferedReader br = new BufferedReader(new FileReader("C:\\Users\\Bob Naessens\\Desktop\\" + args[0] + ".csv"));
+			BufferedReader br = new BufferedReader(new FileReader(args[0]));
 
 			// Read header line
 			line = br.readLine();
@@ -32,21 +32,24 @@ public class TweetAggregation {
 			e.printStackTrace();
 		} 
 		
+		// Get the first instance of a minute
 		Date lastTime = tweetObjects.get(0).getCreated();
 		lastTime.setSeconds(0);
 
+		// Create an arraylist of aggregated tweet objects. These objects hold the datetime for each minute and the count of occurences of that minute.
 		AggregatedTweets at = new AggregatedTweets();
 		ArrayList<AggregatedTweets> AggregatedTweetsObjects = new ArrayList<AggregatedTweets>();
 		
+		// Loop through the original tweets
 		for(int i = 1; i < tweetObjects.size(); i++) {
+			// Get time rounded to minute of tweet
 			Date roundedTime = tweetObjects.get(i).getCreated();
 			roundedTime.setSeconds(0);
 
+			// Check if the minute was the same as the last and add to counter if it is. Otherwise change last time and create new object.
 			if(roundedTime.compareTo(lastTime) == 0) {
 				at.setCount(at.getCount()+1);
-				//System.out.println("here");
 			} else {
-				//System.out.println("here1");
 				at.setDate(lastTime);
 				at.setCount(at.getCount()+1);
 				AggregatedTweetsObjects.add(at);
@@ -55,9 +58,13 @@ public class TweetAggregation {
 			}
 		}
 		
+		// Print results to console. Used mainly for testing.
+		int count = 0;
 		for(int i = 0; i < AggregatedTweetsObjects.size(); i++) {
 			System.out.println("Date: " + AggregatedTweetsObjects.get(i).getDate().toString() + ". Count: " + AggregatedTweetsObjects.get(i).getCount());
+			count += AggregatedTweetsObjects.get(i).getCount();
 		}
+		System.out.println("Total Count: " + count);
 		// Now write the aggregated tweets to a separate csv file
 		
 	}
